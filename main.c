96d4b0bb1a209d2cd9cc901b1074b77c84504615
gabrielafons <141970054+gabrielafons@users.noreply.github.com>
2024-03-22 22:40:26 +0000

    .

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include "tarefas.h"
 
 int main(){
+  int pos = 0;
+  Tarefa tarefas[TOTAL];
+  
   int opcao;
   do {
   printf("\nMenu principal\n");
@@ -11,6 +15,19 @@ int main(){
   printf("Entre com uma opcao: ");
   scanf("%d", &opcao);
   printf("opcao escolhida: %d\n", opcao);
+    if(opcao == 1){
+      int erro = criar(tarefas, &pos);
+    }else if (opcao == 2){
+      int erro = deletar(tarefas, &pos);
+    }else if (opcao == 3){
+      int erro = listar(tarefas, pos);
+    }else if (opcao == 0){
+      printf("Sair\n");
+    }else{
+      printf("opcao invalida");
+    }
+    
+    
   }while (opcao != 0);
   
 
